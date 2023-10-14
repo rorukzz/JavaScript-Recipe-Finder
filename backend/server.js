0a935a719a62802b4088609089353bbf126aeb15
rorukzz <rory.tait.folster@gmail.com>
2023-10-15 00:04:29 +1300

    added folders

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.BACKEND_PORT || 3000;
+
+app.use(express.json());
+
+// Define API endpoints and handle recipe search
+app.get('/api/recipes', async (req, res) => {
+  const ingredients = req.query.ingredients;
+
+  console.log('Received request to /api/recipes');
+  console.log('Ingredients:', ingredients);
+
+  if (!ingredients) {
+    res.status(400).json({ error: 'Ingredients parameter is missing or empty' });
+    return;
+  }
+
+  const ingredientList = ingredients.split(',');
+
+  const apiEndpoint = process.env.RECIPE_API_ENDPOINT; // Define the variable here
+
+  // Log the URL before making the request
+  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredientList.join(',')}&app_id=${process.env.RECIPE_API_ID}&app_key=${process.env.RECIPE_API_KEY}`;
+  console.log('API URL:', apiUrl);
+
+  try {
+    // Make a request to the Edamam API or another recipe API here
+    // Example using Axios:
+    const response = await axios.get(apiEndpoint, {
+      params: {
+        q: ingredientList.join(','),  // Ensure the parameter is a comma-separated string
+        app_id: process.env.RECIPE_API_ID,
+        app_key: process.env.RECIPE_API_KEY, // Use the environment variable directly
+      },
+    });
+
+    const data = response.data;
+
+    // Process and send the recipe data to the frontend
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/script.js
@@ -0,0 +1,110 @@
+// HTML elements
+const addIngredientButton = document.getElementById("add-ingredient");
+const ingredientContainer = document.getElementById("ingredient-container");
+const ingredientCollection = document.getElementById("ingredient-collection");
+const findRecipeButton = document.getElementById("find-recipe");
+
+// Retrieve stored ingredients from localStorage and display them
+const storedIngredients = JSON.parse(localStorage.getItem("ingredients")) || [];
+storedIngredients.forEach(ingredientValue => {
+  const ingredientItem = document.createElement("div");
+  ingredientItem.textContent = ingredientValue;
+  ingredientCollection.appendChild(ingredientItem);
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  ingredientItem.appendChild(removeButton);
+
+  removeButton.addEventListener("click", function() {
+    ingredientCollection.removeChild(ingredientItem);
+    // Remove the ingredient from storedIngredients and update localStorage
+    const index = storedIngredients.indexOf(ingredientValue);
+    if (index > -1) {
+      storedIngredients.splice(index, 1);
+      localStorage.setItem("ingredients", JSON.stringify(storedIngredients));
+    }
+  });
+});
+
+// Event listener for "Add Ingredient" button
+addIngredientButton.addEventListener("click", function() {
+  const inputField = document.getElementById("ingredient-input");
+  const ingredientValue = inputField.value.trim();
+
+  if (ingredientValue !== "") {
+    // Create a new element to display the ingredient in the collection
+    const ingredientItem = document.createElement("div");
+    ingredientItem.textContent = ingredientValue;
+    ingredientCollection.appendChild(ingredientItem);
+
+    // Optionally, you can add a remove button to each ingredient item
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    ingredientItem.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function() {
+      ingredientCollection.removeChild(ingredientItem);
+      // Remove the ingredient from storedIngredients and update localStorage
+      const index = storedIngredients.indexOf(ingredientValue);
+      if (index > -1) {
+        storedIngredients.splice(index, 1);
+        localStorage.setItem("ingredients", JSON.stringify(storedIngredients));
+      }
+    });
+
+    // Store the ingredient in localStorage
+    storedIngredients.push(ingredientValue);
+    localStorage.setItem("ingredients", JSON.stringify(storedIngredients));
+
+    // Clear the input field
+    inputField.value = "";
+  }
+});
+
+// Event listener for "Find Recipe" button
+findRecipeButton.addEventListener("click", function() {
+  const ingredients = [];
+  const ingredientItems = ingredientCollection.children;
+
+  for (let i = 0; i < ingredientItems.length; i++) {
+    ingredients.push(ingredientItems[i].textContent);
+  }
+
+  // Perform the API request using fetch
+  // Replace with the URL of your backend API
+  const apiEndpoint = 'http://localhost:3000/api/recipes';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ingredients }),
+  })
+    .then(async response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      // Process and display the recipe results
+      const recipeResults = document.getElementById("recipe-results");
+      recipeResults.innerHTML = '';
+
+      if (data.recipes && data.recipes.length > 0) {
+        data.recipes.forEach(recipe => {
+          const recipeItem = document.createElement("div");
+          for (const prop in recipe) {
+            if (recipe.hasOwnProperty(prop)) {
+              const recipeProperty = document.createElement("p");
+              recipeProperty.textContent = `${prop}: ${recipe[prop]}`;
+              recipeItem.appendChild(recipeProperty);
+            }
+          }
+          recipeResults.appendChild(recipeItem);
+        });
+      } else {
+        recipeResults.textContent = "No recipes found with these ingredients.";
+      }
+    })
+    .catch(error => console.error(error));
+});
